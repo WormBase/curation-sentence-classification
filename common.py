3e8d9ff27f4589ef52a567310b1e54f71ee95da8
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2024-11-18 13:14:47 -0800

    updated scripts to add pmids and use one additional subclass

--- common.py
+++ common.py
@@ -19,3 +19,35 @@ def get_papers_from_cs_form(datatype, user, password, method,
         if m:
             curated_papers = set(m.group(1).split())
     return list(curated_papers)
+
+
+import requests
+
+
+def get_pmid_from_wbpaperid(wbpaperid):
+    # Ensure the ID has 8 digits by padding with zeros if necessary
+    wbpaperid = wbpaperid.zfill(8)
+
+    # Ensure the ID is in the format 'WB:WBPaper' if not already present
+    if not wbpaperid.startswith('WB:WBPaper'):
+        wbpaperid = f'WB:WBPaper{wbpaperid}'
+
+    # URL for the API request
+    url = f'https://stage-literature-rest.alliancegenome.org/reference/by_cross_reference/{wbpaperid}'
+
+    # Headers for the API request
+    headers = {
+        'accept': 'application/json'
+    }
+
+    # Make the API request
+    response = requests.get(url, headers=headers)
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        data = response.json()
+        # Extract the PMID from the crossReferences list
+        for cross_reference in data.get('cross_references', []):
+            if cross_reference.get('curie_prefix') == 'PMID':
+                return cross_reference.get('curie')
+    return None
